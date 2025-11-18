86be1520115f02574cc870ae9c27285477dcaa63
Valarie Lim <vallimyh92@gmail.com>
2025-11-18 08:55:54 +0800

    upload html, css, js files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// navigation burger menu
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+}
+
+// sticky header scroll effect
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('main-header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+
+
+
